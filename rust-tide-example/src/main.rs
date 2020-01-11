3a5fc3d41856700e3e034797d4e5608770257000
Fuyang Liu <liufuyang@users.noreply.github.com>
2020-01-11 22:18:18 +0100

    Use Arc instand of lazy-static

--- rust-tide-example/src/main.rs
+++ rust-tide-example/src/main.rs
@@ -1,69 +1,56 @@
-#[macro_use]
-extern crate lazy_static;
 
 use std::collections::HashMap;
 use std::sync::Mutex;
+use std::sync::Arc;
 
 use async_std::io;
 use async_std::task;
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
-struct Cat {
-    name: String,
-}
-
-lazy_static! {
-    static ref MAP: Mutex<HashMap<String, u32>> = {
-        let mut m: HashMap<String, u32> = HashMap::new();
-        m.insert("add1".to_owned(), 0);
-        m.insert("add2".to_owned(), 0);
-        m.insert("add3".to_owned(), 0);
-        Mutex::new(m)
-    };
-}
-
 fn main() -> io::Result<()> {
     task::block_on(async {
         let mut app = tide::new();
 
-        app.at("/show").get(|_req: tide::Request<()>| {
+        let mutex: Mutex<HashMap<String, u32>> = {
+            let mut m: HashMap<String, u32> = HashMap::new();
+            m.insert("add1".to_owned(), 0);
+            m.insert("add2".to_owned(), 0);
+            m.insert("add3".to_owned(), 0);
+            Mutex::new(m)
+        };
+        let arc = Arc::new(mutex);
+
+        let _arc = Arc::clone(&arc);
+        app.at("/show").get(move |_req: tide::Request<()>| {
+            let map= Arc::clone(&_arc);
             async move {
-                tide::Response::new(200).body_json(&*MAP).unwrap()
+                tide::Response::new(200).body_json(&*map).unwrap()
             }
         });
 
-        app.at("/add").get(|_req: tide::Request<()>| {
-            async {
-                let map = &mut *MAP.lock().unwrap();
+        let _arc = Arc::clone(&arc);
+        app.at("/add").get(move |_req: tide::Request<()>| {
+            let map= Arc::clone(&_arc);
+            async move {
+                let map = &mut *map.lock().unwrap();
                 map.get_mut("add1").map(|v| *v += 1);
                 map.get_mut("add2").map(|v| *v += 2);
                 map.get_mut("add3").map(|v| *v += 3);
 
-                tide::Response::new(200).body_json(map).unwrap()
+                tide::Response::new(200).body_json(&*map).unwrap()
             }
         });
 
-        app.at("/reset").get(|_req: tide::Request<()>| {
-            async {
-                let map = &mut *MAP.lock().unwrap();
+        let _arc = Arc::clone(&arc);
+        app.at("/reset").get(move |_req: tide::Request<()>| {
+            let map= Arc::clone(&_arc);
+            async move {
+                let map = &mut *map.lock().unwrap();
                 *map.get_mut("add1").unwrap() = 0;
                 *map.get_mut("add2").unwrap() = 0;
                 *map.get_mut("add3").unwrap() = 0;
 
-                tide::Response::new(200).body_json(map).unwrap()
-            }
-        });
-
-        app.at("/submit").post(|mut req: tide::Request<()>| {
-            async move {
-                let cat: Cat = req.body_json().await.unwrap();
-                println!("cat name: {}", cat.name);
-
-                let cat = Cat {
-                    name: "chashu".into(),
-                };
-                tide::Response::new(200).body_json(&cat).unwrap()
+                tide::Response::new(200).body_json(&*map).unwrap()
             }
         });
 
