12637e0877b0601abe7e7886431dbee16781baf2
Fuyang Liu <fuyangl@spotify.com>
2020-01-17 17:02:47 +0100

    Proto package issue

--- trackinfo-tonic/src/server.rs
+++ trackinfo-tonic/src/server.rs
@@ -1,6 +1,8 @@
-use tonic::{Request, Response, Status, transport::Server};
+use tonic::{metadata::MetadataValue, Request, Response, Status, transport::Channel, transport::Server};
 
-use track_info::{TrackToStringRequest, TitleAndArtist};
+use metadata::GetTrackRequest;
+use metadata_v1beta1::metadata_client::MetadataClient;
+use track_info::{TitleAndArtist, TrackToStringRequest};
 use track_info::golden_path_example_service_server::{GoldenPathExampleService, GoldenPathExampleServiceServer};
 
 pub mod track_info {
@@ -11,8 +13,14 @@ pub mod metadata {
     tonic::include_proto!("spotify.metadata");
 }
 
-#[derive(Default)]
-pub struct Service {}
+pub mod metadata_v1beta1 {
+    tonic::include_proto!("spotify.metadata.v1beta1");
+}
+
+
+pub struct Service {
+    metadata_client: MetadataClient<Channel>
+}
 
 #[tonic::async_trait]
 impl GoldenPathExampleService for Service {
@@ -24,6 +32,23 @@ impl GoldenPathExampleService for Service {
 
         let request = request.into_inner(); // We must use .into_inner() as the fields of gRPC requests and responses are private
 
+        let track_request = tonic::Request::new(GetTrackRequest {
+            gid: "18c5dd0479ad446b9f1bbbcfea8ce59e".into(),
+            country: "US".into(),
+            catalogue: "free".into(),
+            accept_language: vec![],
+            preview: true,
+            view: 0,
+            etag: vec![],
+        });
+
+        println!("{:?}", track_request);
+        //  tonic's clients are always backed by channels so cloning them is cheap
+        let mut client = self.metadata_client.clone();
+        let response = client.get_track(track_request).await?;
+
+        println!("RESPONSE={:?}", response);
+
         let reply = TitleAndArtist {
             track_string: format!("Hello"),
         };
@@ -34,7 +59,18 @@ impl GoldenPathExampleService for Service {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50052".parse().unwrap();
-    let service = Service::default();
+
+    let channel = Channel::from_static("http://gew1-metadataproxygrpc-b-m9mx.gew1.spotify.net.:8080").connect().await?;
+    let token = MetadataValue::from_str("xxx")?;
+    let metadata_client = MetadataClient::with_interceptor(channel, move |mut req: Request<()>| {
+        println!("inserting key");
+        req.metadata_mut().insert("authorization", token.clone());
+        println!("req updated");
+        Ok(req)
+    });
+    let service = Service {
+        metadata_client,
+    };
 
     println!("Server listening on {}", addr);
 
